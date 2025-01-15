8a12f6b61bf0dc6cae9c6a2fe4a933ff4704fdbe
sfg-tech <sumitfitnessgroup@gmail.com>
2025-01-15 21:44:49 +0530

    change port number

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+// import { HttpExceptionFilter } from './common/exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(process.env.PORT);
 }
 bootstrap();
