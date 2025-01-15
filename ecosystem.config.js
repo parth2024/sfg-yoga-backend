c858c46cda5d402e707f0d7468e04f0da7b7339c
sfg-tech <sumitfitnessgroup@gmail.com>
2025-01-15 23:30:16 +0530

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 export const apps = [
     {
-        name: "sfg-backend",
-        script: "./src/main.ts",
-        interpreter: "/usr/bin/node",
+      name: "sfg-backend",
+      script: "./src/main.ts",
+      interpreter: "/usr/bin/ts-node",  // Use ts-node as the interpreter for TypeScript
+      watch: true,  // Optional: to watch for changes in your files and auto-restart
     },
-];
-  
+  ];
