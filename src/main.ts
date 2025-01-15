e0645f71974ffe71b151d53b283826e56266f668
sfg-tech <sumitfitnessgroup@gmail.com>
2025-01-15 21:49:10 +0530

    fix build issue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-// import { HttpExceptionFilter } from './common/exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
