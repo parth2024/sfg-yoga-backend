d85b49d2d4093f11afe2495b6ebe64fa6bb2d0b8
sfg-tech <sumitfitnessgroup@gmail.com>
2025-01-18 00:40:13 +0530

    allow origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow specific origin
+    origin: ['http://localhost:3000','https://www.sfgyoga.in/admin/dashboard','https://www.sfgyoga.in'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     credentials: true, // If you need to allow cookies or authorization headers
   });
